a2dbc3ac84681f7d25c0228b963aa15fd27a48a5
Dhaval Bharodiya <43804554+BDP-Dhaval@users.noreply.github.com>
2020-07-21 17:01:17 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
 $db_username = 'root';
-$db_password = '';
-$db_name = 'bolt';
-$db_host = 'localhost';
+$db_password = 'Admin@123';
+$db_name = 'ecommerce';
+$db_host = 'http://18.222.187.55/';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
