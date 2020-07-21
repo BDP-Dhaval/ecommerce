55b62500658192005558635ce65e9cf3ea8e30f8
Dhaval Bharodiya <43804554+BDP-Dhaval@users.noreply.github.com>
2020-07-21 17:18:59 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ $currency = '₹';
 $db_username = 'root';
 $db_password = 'Admin@123';
 $db_name = 'ecommerce';
-$db_host = 'http://18.222.187.55/';
+$db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
